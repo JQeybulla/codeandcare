cdb8ee9df22c422a3b5a20b9a99e10309401c8ec
Qeybulla Əliverdiyev <q.aliverdiyev@asan.local>
2024-08-28 15:46:15 +0400

    kdkdkd

--- shared/sections/header/Header.component.tsx
+++ shared/sections/header/Header.component.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.scss';
 import Image from "next/image";
 import logo from './../../../public/CODE.svg';
 import Link from "next/link";
-import { usePathname } from 'next/navigation';
+import {usePathname, useRouter} from 'next/navigation';
 import Hamburger from 'hamburger-react'
 import {useState} from "react";
 
@@ -11,7 +11,12 @@ import {useState} from "react";
 export default function HeaderComponent () {
   const pathname = usePathname();
   const [isOpen, setOpen] = useState(false)
-  console.log(pathname)
+  const router  = useRouter();
+
+  const navigate = (link: string) => {
+    router.push(link);
+    setOpen(false);
+  }
   return (
       <>
           <div className={styles.header}>
@@ -39,17 +44,14 @@ export default function HeaderComponent () {
               </div>
           </div>
           {isOpen && <div className={`${styles.mobileHeader} ${isOpen ? styles.open : ''}`}>
-              <Link href='/directions'><p className={`${pathname === '/directions' ? styles.active : ''}`}>Tədris
-                  istiqamətləri</p></Link>
-              <Link href='/about-us'><p
-                  className={`${pathname === '/about-us' ? styles.active : ''}`}>Haqqımızda</p></Link>
+            <p onClick={() => navigate('/directions')} className={`${pathname === '/directions' ? styles.active : ''}`}>Tədris
+              istiqamətləri</p>
+            <p onClick={() => navigate('/about-us')}
+              className={`${pathname === '/about-us' ? styles.active : ''}`}>Haqqımızda</p>
               {/*<Link href='/news'><p className={`${pathname === '/news' ? styles.active : ''}`}>Xəbərlər</p></Link>*/}
               {/*<Link href='/blogs'><p className={`${pathname === '/blogs' ? styles.active : ''}`}>Blog</p></Link>*/}
-              <Link href='/contact-us'><p className={`${pathname === '/contact-us' ? styles.active : ''}`}>Əlaqə</p>
-              </Link>
-              <Link href='/apply'>
-                  <button>Müraciət et</button>
-              </Link>
+            <p onClick={() => navigate('/contact-us')} className={`${pathname === '/contact-us' ? styles.active : ''}`}>Əlaqə</p>
+            <button onClick={() => navigate('/apply')}>Müraciət et</button>
           </div>}
       </>
   )
