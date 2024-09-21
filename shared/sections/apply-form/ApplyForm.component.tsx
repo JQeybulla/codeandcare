6ebb91f784b852409e233d9ff05c12147a905439
JQeybulla <geybulla.aliverdiyev.std@bhos.edu.az>
2024-09-22 00:28:43 +0400

    added new images

--- shared/sections/apply-form/ApplyForm.component.tsx
+++ shared/sections/apply-form/ApplyForm.component.tsx
@@ -75,7 +75,7 @@ export default function ApplyFormComponent() {
     }
     console.log(submittingObject);
 
-    await axios.post('https://codeandcarebackend-1.onrender.com/api/apply', submittingObject)
+    await axios.post('http://46.101.246.103:3001/api/apply', submittingObject)
       .then(res => {
         if (res.status === 201) {
           toast.success('Müraciətiniz qeydə alındı!', {
